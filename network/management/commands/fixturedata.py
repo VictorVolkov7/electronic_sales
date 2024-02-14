496ecbc0954c9db6a37162ec2b53319e7e460b70
Victor Volkov <vektorn1212@gmail.com>
2024-02-14 10:46:40 +0200

    added custom command for loading fixtures

---
+++ network/management/commands/fixturedata.py
@@ -0,0 +1,23 @@
+import os
+
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+
+class Command(BaseCommand):
+    """
+    Loads data from JSON files.
+    """
+    help = 'Loads data from fixtures dir'
+    fixtures_dir = 'fixtures'
+    loaddata_command = 'loaddata'
+    filenames = [
+        'product',
+        'network_links',
+    ]
+
+    def handle(self, *args, **options):
+        for fixture_filename in self.filenames:
+            call_command(
+                self.loaddata_command, os.path.join(self.fixtures_dir, f'{fixture_filename}.json')
+            )
